3250ea1557c72e7c0363f88248c267841f335175
Barney Dono <sovietspy2@users.noreply.github.com>
2023-10-10 19:12:56 +0200

    creating basic html layout
    Co-authored-by: mttwz <mttwz@users.noreply.github.com>

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,1 +1,50 @@
+//import { v4 as uuidv4 } from 'uuid';
+
+
+
 console.log("loaded")
+
+
+function createCell() {
+
+    var cellElement = document.createElement("div");
+    cellElement.className = "cell";
+    cellElement.textContent="haho";
+
+    return cellElement;
+}
+
+//const uuid = uuidv4();
+
+function generateCells(rows, columns){
+
+    const grid = document.getElementById("grid");
+
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < columns; j++) {
+            var cell = createCell();
+            grid.appendChild(cell);
+        }
+    }
+}
+
+function getJoinCode(){
+    let code = document.getElementById("joinCode").value;
+    return code;
+}
+
+
+const joinButton = document.getElementById("join");
+
+joinButton.addEventListener("click", () => {
+    console.log("clicked join button");
+    console.log(getJoinCode());
+});
+
+const hostButton = document.getElementById("host");
+
+hostButton.addEventListener("click", () => {
+    console.log("clicked host button");
+});
+
+generateCells(7,6);
